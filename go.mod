44b89e49d23d493706d83789ed01d58d5e9b21b0
piftai <piftaiwork@gmail.com>
2026-05-16 04:07:29 +0300

    test ci pipeline.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module prodadim
 
 go 1.26.3
+
+require github.com/stretchr/testify v1.11.1
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func Test_sum(t *testing.T) {
+	assert.Equal()
 }
