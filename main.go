0737b377ddf9c8c48c7dbb2206bca5397abb86c7
piftai <piftaiwork@gmail.com>
2026-05-16 04:13:05 +0300

    fix: edited main.go.

--- main.go
+++ main.go
@@ -2,19 +2,9 @@ package main
 
 import (
 	"fmt"
-	"testing"
-
-	"github.com/stretchr/testify"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
-}
 
-func sum(a, b int) int {
-	return a + b
-}
-
-func Test_sum(t *testing.T) {
-	assert.Equal()
 }
